50a817fb4e8e9c08451611497494e4ac577b4044
chanmmn <33271651+chanmmn@users.noreply.github.com>
2019-05-10 19:30:34 +0800

    Microsoft Graph Console Application
    Check in Microsoft Graph Console Application

---
+++ ConAppGraph/ConAppGraph/Program.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Graph;
+using Microsoft.Graph.Auth;
+using Microsoft.Identity.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConAppGraph
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Run().GetAwaiter().GetResult();
+            Console.Read();
+        }
+
+        public static async Task Run()
+        {
+            var clientId = "Application ID";
+            var scopes = new List<string>() { "User.ReadBasic.All" }.ToArray();
+
+            IPublicClientApplication clientApplication = InteractiveAuthenticationProvider.CreateClientApplication(clientId);
+            InteractiveAuthenticationProvider authProvider = new InteractiveAuthenticationProvider(clientApplication, scopes);
+
+            GraphServiceClient graphClient = new GraphServiceClient(authProvider);
+
+            var users = await graphClient.Users
+                .Request()
+                .Select(e => new {
+                    e.DisplayName,
+                    e.GivenName,
+                    e.PostalCode
+                })
+                .GetAsync();
+
+            foreach (var user in users)
+            {
+                Console.WriteLine(user.GivenName);
+            }
+        }
+    }
+}
